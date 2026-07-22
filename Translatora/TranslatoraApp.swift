b9408f3bcab3a3ee1c83de87bb89c1bd89139a45
Kaiyi <skic6039@gmail.com>
2026-07-22 10:45:34 +0800

    Initial Commit

---
+++ Translatora/TranslatoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatoraApp.swift
+//  Translatora
+//
+//  Created by 孙凯一 on 2026/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TranslatoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
